85fdfba5e6c8a99ac7b9cb13754727e30f80bb0c
Farhaana Mujid <mujidfarhaana@yahoo.com>
2020-10-20 17:00:45 -0500

    hw2

---
+++ src/app/request-component/request-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-component',
+  templateUrl: './request-component.component.html',
+  styleUrls: ['./request-component.component.css']
+})
+export class RequestComponentComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
