78c753f20696e24d934cf662d15eba826d3f2c4e
Farhaana Mujid <mujidfarhaana@yahoo.com>
2020-11-03 05:38:15 -0600

    hw3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Farhaana Mujid';
-
-  numOfTravelers:number = 0;
-
-
 }
-
-
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RequestComponentComponent } from './request-component/request-component.component';
+import { AboutComponentComponent } from './about-component/about-component.component';
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+
+const routes: Routes = [
+
+ { path: 'about', component:AboutComponentComponent },
+ { path: 'flightrequest', component:RequestComponentComponent },
+ { path: '', component:HomeComponentComponent },
+
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RequestComponentComponent
+    RequestComponentComponent,
+    AboutComponentComponent,
+    NavbarComponentComponent,
+    HomeComponentComponent
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
